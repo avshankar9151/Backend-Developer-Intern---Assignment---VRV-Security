d0ff8312220b112efcc2bded2df5b96085382f91
VIJAY SHANKAR VISHWAKARMA <avshankar9151@gmail.com>
2024-11-29 22:20:50 +0530

    adding static root directory

--- rbac_manage/settings.py
+++ rbac_manage/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = []
 
 # Default primary key field type
